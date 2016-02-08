a53eb54910560d3ffc4b42740de4b9bd48d7ae9d
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-02-08 18:00:21 +0100

    Implemented CollectionVariable

--- CollectionVariable/CollectionVariable.swift
+++ CollectionVariable/CollectionVariable.swift
@@ -1,6 +1,5 @@
 import Foundation
-import ReactiveCocoa
-import Result
+import RxSwift
 
 public enum CollectionChange<T> {
     case Remove(Int, T)
@@ -24,79 +23,59 @@ public enum CollectionChange<T> {
     }
 }
 
-public final class MutableCollectionProperty<T>: PropertyType {
 
-    public typealias Value = [T]
+public final class CollectionVariable<T> {
+    
+    // MARK: - Attributes
+    
+    private let _changesSubject: BehaviorSubject<CollectionChange<T>>
+    private let _subject: BehaviorSubject<[T]>
+    private var _lock = NSRecursiveLock()
+    public var observable: Observable<[T]> { return _subject.asObservable() }
+    public var changesObservable: Observable<CollectionChange<T>> { return _changesSubject.asObservable() }
+    private var _value: [T] {
+        get {
+            _lock.lock(); defer { _lock.unlock() }
+            return self._value
+        }
+        set(newValue) {
+            _lock.lock()
+            self._value = newValue
+            _lock.unlock()
+        }
+    }
 
     
-    // MARK: - Private attributes
-
-    private let _valueObserver: Signal<Value, NoError>.Observer
-    private let _valueObserverSignal: Signal<Value, NoError>.Observer
-    private let _changesObserver: Signal<CollectionChange<Value.Element>, NoError>.Observer
-    private let _changesObserverSignal: Signal<CollectionChange<Value.Element>, NoError>.Observer
-    private var _value: Value
-    private let _lock = NSRecursiveLock()
-
-    // MARK: - Public Attributes
-
-    public var producer: SignalProducer<Value, NoError>
-    public var signal: Signal<Value, NoError>
-    public var changes: SignalProducer<CollectionChange<Value.Element>, NoError>
-    public var changesSignal: Signal<CollectionChange<Value.Element>, NoError>
-    public var value: Value {
-        get {
-            let value = _value
-            return value
+    // MARK: - Init
+    
+    public init(value: [T]) {
+        var initialChanges: [CollectionChange<T>] = []
+        for (index, element) in value.enumerate() {
+            initialChanges.append(.Insert(index, element))
         }
-        set {
-            _value = newValue
-            _valueObserver.sendNext(newValue)
-            _changesObserver.sendNext(.Composite(newValue.mapWithIndex{CollectionChange.Insert($0, $1)}))
-        }
-    }
-
-    // MARK: - Init/Deinit
-
-    public init(_ initialValue: Value) {
-        _lock.name = "org.reactivecocoa.ReactiveCocoa.MutableCollectionProperty"
-        _value = initialValue
-        (producer, _valueObserver) = SignalProducer<Value, NoError>.buffer(1)
-        (changes, _changesObserver) = SignalProducer<CollectionChange<Value.Element>, NoError>.buffer(1)
-        (signal, _valueObserverSignal) = Signal<Value, NoError>.pipe()
-        (changesSignal, _changesObserverSignal) = Signal<CollectionChange<Value.Element>, NoError>.pipe()
-    }
-
-    deinit {
-        _valueObserver.sendCompleted()
-        _valueObserverSignal.sendCompleted()
-        _changesObserver.sendCompleted()
-        _changesObserverSignal.sendCompleted()
+        _changesSubject = BehaviorSubject(value: .Composite(initialChanges))
+        _subject = BehaviorSubject(value: value)
     }
     
     
     // MARK: - Public
-
+    
     public func removeFirst() {
         if (_value.count == 0) { return }
         _lock.lock()
         let deletedElement = _value.removeFirst()
-        _changesObserver.sendNext(.Remove(0, deletedElement))
-        _changesObserverSignal.sendNext(.Remove(0, deletedElement))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Remove(0, deletedElement))
+        _subject.onNext(_value)
         _lock.unlock()
     }
-
+    
     public func removeLast() {
         _lock.lock()
         if (_value.count == 0) { return }
         let index = _value.count - 1
         let deletedElement = _value.removeLast()
-        _changesObserver.sendNext(.Remove(index, deletedElement))
-        _changesObserverSignal.sendNext(.Remove(index, deletedElement))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Remove(index, deletedElement))
+        _subject.onNext(_value)
         _lock.unlock()
     }
     
@@ -104,30 +83,24 @@ public final class MutableCollectionProperty<T>: PropertyType {
         _lock.lock()
         let copiedValue = _value
         _value.removeAll()
-        _changesObserver.sendNext(.Composite(copiedValue.mapWithIndex{CollectionChange.Remove($0, $1)}))
-        _changesObserverSignal.sendNext(.Composite(copiedValue.mapWithIndex{CollectionChange.Remove($0, $1)}))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Composite(copiedValue.mapWithIndex{CollectionChange.Remove($0, $1)}))
+        _subject.onNext(_value)
         _lock.unlock()
     }
-
+    
     public func removeAtIndex(index: Int) {
         _lock.lock()
         let deletedElement = _value.removeAtIndex(index)
-        _changesObserver.sendNext(CollectionChange.Remove(index, deletedElement))
-        _changesObserverSignal.sendNext(CollectionChange.Remove(index, deletedElement))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(CollectionChange.Remove(index, deletedElement))
+        _subject.onNext(_value)
         _lock.unlock()
     }
     
     public func append(element: T) {
         _lock.lock()
         _value.append(element)
-        _changesObserver.sendNext(.Insert(_value.count - 1, element))
-        _changesObserverSignal.sendNext(.Insert(_value.count - 1, element))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Insert(_value.count - 1, element))
+        _subject.onNext(_value)
         _lock.unlock()
     }
     
@@ -135,20 +108,16 @@ public final class MutableCollectionProperty<T>: PropertyType {
         _lock.lock()
         let count = _value.count
         _value.appendContentsOf(elements)
-        _changesObserver.sendNext(.Composite(elements.mapWithIndex{CollectionChange.Insert(count + $0, $1)}))
-        _changesObserverSignal.sendNext(.Composite(elements.mapWithIndex{CollectionChange.Insert(count + $0, $1)}))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Composite(elements.mapWithIndex{CollectionChange.Insert(count + $0, $1)}))
+        _subject.onNext(_value)
         _lock.unlock()
     }
     
     public func insert(newElement: T, atIndex index: Int) {
         _lock.lock()
         _value.insert(newElement, atIndex: index)
-        _changesObserver.sendNext(.Insert(index, newElement))
-        _changesObserverSignal.sendNext(.Insert(index, newElement))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Insert(index, newElement))
+        _subject.onNext(_value)
         _lock.unlock()
     }
     
@@ -163,14 +132,17 @@ public final class MutableCollectionProperty<T>: PropertyType {
             deletesComposite.append(.Remove(subRange.startIndex + index, replacedElement))
             insertsComposite.append(.Insert(subRange.startIndex + index, element))
         }
-        _changesObserver.sendNext(.Composite(deletesComposite))
-        _changesObserverSignal.sendNext(.Composite(deletesComposite))
-        _changesObserver.sendNext(.Composite(insertsComposite))
-        _changesObserverSignal.sendNext(.Composite(insertsComposite))
-        _valueObserver.sendNext(_value)
-        _valueObserverSignal.sendNext(_value)
+        _changesSubject.onNext(.Composite(deletesComposite))
+        _changesSubject.onNext(.Composite(insertsComposite))
+        _subject.onNext(_value)
         _lock.unlock()
     }
+    
+    deinit {
+        _subject.onCompleted()
+        _changesSubject.onCompleted()
+    }
+    
 }
 
 extension Array {
